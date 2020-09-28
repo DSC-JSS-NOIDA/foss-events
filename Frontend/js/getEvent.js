3ce1fa7928dde51b8bb7257002443b02388217c2
JatinAgrawal94 <jatinagrawal0801@gmail.com>
2020-09-28 11:23:30 +0530

    Website made responsive

--- Frontend/js/getEvent.js
+++ Frontend/js/getEvent.js
@@ -1,5 +1,5 @@
 // Fetch data from events.json file
-/*
+
 fetch("../data/events.json")
   .then((res) => res.json())
   .then((res) => {
@@ -7,131 +7,63 @@ fetch("../data/events.json")
     for (let data in res) {
       let emptyDiv = document.createElement("div");
       emptyDiv.className = "empty_div";
+
       let eventCard = document.createElement("div");
       eventCard.className = "event_card";
+
       let eventTitle = document.createElement("div");
+
       let heading = document.createElement("h3");
       heading.innerText = res[data].title;
       heading.className = "event_title";
       eventTitle.appendChild(heading);
+
       let startDate = document.createElement("span");
       startDate.className = "date";
       startDate.innerHTML = `<b>Starts:</b> ${res[data].start}`;
+
       let endDate = document.createElement("span");
       endDate.className = "date";
       endDate.innerHTML = `<b>Ends:</b> ${res[data].start}`;
+
       let eventDetails = document.createElement("div");
       eventDetails.className = "event_details";
+
       let eventLink = document.createElement("a");
       Object.assign(eventLink, {
         href: `https://${res[data].website}`,
         target: "_blank",
         rel: "noopener noreferrer",
         className:'btn btn-primary link'
+
       });
       let link = document.createElement("h6");
       link.innerText = "More Info";
       eventLink.appendChild(link);
+
       let organisation = document.createElement("p");
       organisation.innerHTML = `<b>Organisation: ${res[data].organisation}`;
+      organisation.className="organisation_name";
+
       let loc = document.createElement("h5");
       loc.className = "location";
       loc.innerText = res[data].location;
+
       eventDetails.append(startDate, endDate, organisation, loc,eventLink);
       eventCard.append(eventTitle, eventDetails);
       emptyDiv.appendChild(eventCard);
       cardContainer.appendChild(emptyDiv);
     }
   });
-*/
-const tryButton=document.getElementById('tryal')
-tryButton.addEventListener("click",()=>{
-  console.log("hello world");
-  test();
-})
 
-const res=[
-            {
-              "title": "Test 1",
-              "website": "yourlink.com",
-              "start": "dd/mm/yy",
-              "end": "dd/mm/yy",
-              "organisation": "DSC JSS",
-              "location": "Online"
-            },
-            {
-              "title": "Test 2",
-              "website": "example.com",
-              "start": "dd/mm/yy",
-              "end": "dd/mm/yy",
-              "organisation": "DSC JSS",
-              "location": "Online"
-            },
-            {
-              "title": "Test 3",
-              "website": "example.com",
-              "start": "dd/mm/yy",
-              "end": "dd/mm/yy",
-              "organisation": "DSC JSS",
-              "location": "Online"
-            },
-            {
-              "title": "Test 4",
-              "website": "example.com",
-              "start": "dd/mm/yy",
-              "end": "dd/mm/yy",
-              "organisation": "DSC JSS",
-              "location": "Online"
-            },
-            {
-              "title": "Test 5",
-              "website": "example.com",
-              "start": "dd/mm/yy",
-              "end": "dd/mm/yy",
-              "organisation": "DSC JSS",
-              "location": "Online"
-            }
-          ];
-
-
-function test(){
-  let cardContainer = document.getElementsByClassName("card-container")[0];
-  for (let data in res) {
-    let emptyDiv = document.createElement("div");
-    emptyDiv.className = "empty_div";
-    let eventCard = document.createElement("div");
-    eventCard.className = "event_card";
-    let eventTitle = document.createElement("div");
-    let heading = document.createElement("h3");
-    heading.innerText = res[data].title;
-    heading.className = "event_title";
-    eventTitle.appendChild(heading);
-    let startDate = document.createElement("span");
-    startDate.className = "date";
-    startDate.innerHTML = `<b>Starts:</b> ${res[data].start}`;
-    let endDate = document.createElement("span");
-    endDate.className = "date";
-    endDate.innerHTML = `<b>Ends:</b> ${res[data].start}`;
-    let eventDetails = document.createElement("div");
-    eventDetails.className = "event_details";
-    let eventLink = document.createElement("a");
-    Object.assign(eventLink, {
-      href: `https://${res[data].website}`,
-      target: "_blank",
-      rel: "noopener noreferrer",
-      className:'btn btn-primary link'
-    });
-    let link = document.createElement("h6");
-    link.innerText = "More Info";
-    eventLink.appendChild(link);
-    let organisation = document.createElement("p");
-    organisation.innerHTML = `<b>Organisation: ${res[data].organisation}`;
-    let loc = document.createElement("h5");
-    loc.className = "location";
-    loc.innerText = res[data].location;
-    eventDetails.append(startDate, endDate, organisation, loc,eventLink);
-    eventCard.append(eventTitle, eventDetails);
-    emptyDiv.appendChild(eventCard);
-    cardContainer.appendChild(emptyDiv);
+function checkForEmpty(){
+  if($('.card-container').is(':empty')){
+    $('footer').css('top','100%');
   }
 }
+function checkForFilled(){
+  if(! $('.card-container').is(':empty')){
+    $('footer').css('top','auto');
+  }
+}
+checkForEvents();
