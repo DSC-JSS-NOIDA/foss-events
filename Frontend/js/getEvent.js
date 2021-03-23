08524165cb65e1e95d74cfb81749de49fdf72258
osamamagdy <59124937+osamamagdy@users.noreply.github.com>
2021-03-23 10:36:42 +0200

    Revert "adding a similarity score"
    This reverts commit 3b8e755bc19585f5b369a2b051653465bc1b2d71.

--- Frontend/js/getEvent.js
+++ Frontend/js/getEvent.js
@@ -61,65 +61,19 @@ fetch("./Frontend/data/events.json")
 // search an event
 let search = document.querySelector(".form-control");
 search.addEventListener("keyup", searchTerm);
-//function to calculate the similarity score between two strings
-function similarity(s1, s2) {
-      var longer = s1;
-      var shorter = s2;
-      if (s1.length < s2.length) {
-        longer = s2;
-        shorter = s1;
-      }
-      var longerLength = longer.length;
-      if (longerLength == 0) {
-        return 1.0;
-      }
-      return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
-    }
-
-    function editDistance(s1, s2) {
-      s1 = s1.toLowerCase();
-      s2 = s2.toLowerCase();
-
-      var costs = new Array();
-      for (var i = 0; i <= s1.length; i++) {
-        var lastValue = i;
-        for (var j = 0; j <= s2.length; j++) {
-          if (i == 0)
-            costs[j] = j;
-          else {
-            if (j > 0) {
-              var newValue = costs[j - 1];
-              if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                newValue = Math.min(Math.min(newValue, lastValue),
-                  costs[j]) + 1;
-              costs[j - 1] = lastValue;
-              lastValue = newValue;
-            }
-          }
-        }
-        if (i > 0)
-          costs[s2.length] = lastValue;
-      }
-      return costs[s2.length];
-    }
 
 //function to search the event
 function searchTerm(e) {
   let eventList = document.querySelectorAll(".empty_div");
   let input = e.target.value.toLowerCase();
-  if (input !="") {
-    
-    Array.from(eventList).forEach((eventItem) => {
-      let toSearch = eventItem.childNodes[0].children[0].childNodes[0].innerText;
-      console.log(similarity(toSearch.toLowerCase(), input));
-      console.log(toSearch.toLowerCase());
-      if ( toSearch.toLowerCase().indexOf(input) != -1||similarity(toSearch.toLowerCase(), input) > 0.08 ) {
-        eventItem.style.display = "block";
-      } else {
-        eventItem.style.display = "none";
-      }
-    });
-  }
+  Array.from(eventList).forEach((eventItem) => {
+    let toSearch = eventItem.childNodes[0].children[0].childNodes[0].innerText;
+    if (toSearch.toLowerCase().indexOf(input) != -1) {
+      eventItem.style.display = "block";
+    } else {
+      eventItem.style.display = "none";
+    }
+  });
 }
 
 const toggleSwitch = document.querySelector(".custom-control-input");
