21ecd01d18416f4a90d8d78d9051b3ac82f004c4
osamamagdy <59124937+osamamagdy@users.noreply.github.com>
2021-03-25 17:52:08 +0200

    Sorting and ubdating the dom by score

--- Frontend/js/getEvent.js
+++ Frontend/js/getEvent.js
@@ -1,82 +1,77 @@
+//Function to load the data of json file into the website
+function loadJSON(res_file){
+  let cardContainer = document.getElementsByClassName("card-container")[0];
+// As long as cardContainer has a child node, remove it
+while (cardContainer.hasChildNodes()) {  
+  cardContainer.removeChild(cardContainer.firstChild);
+}
+  for (let data in res_file) {
+    let emptyDiv = document.createElement("div");
+    emptyDiv.className = "empty_div";
+    let eventCard = document.createElement("div");
+    eventCard.className = "event_card";
+    let eventTitle = document.createElement("div");
+    let heading = document.createElement("h3");
+    heading.innerText = res_file[data].title;
+    heading.className = "event_title";
+    eventTitle.appendChild(heading);
+    let startDate = document.createElement("span");
+    startDate.className = "date";
+    startDate.innerHTML = `<b>Starts:</b> ${res_file[data].start}`;
+    let endDate = document.createElement("span");
+    endDate.className = "date";
+    endDate.innerHTML = `<b>Ends:</b> ${res_file[data].end}`;
+    let eventDetails = document.createElement("div");
+    eventDetails.className = "event_details";
+    let eventLink = document.createElement("a");
+    Object.assign(eventLink, {
+      href: `https://${res_file[data].website}`,
+      target: "_blank",
+      rel: "noopener noreferrer",
+      className: "btn btn-primary link",
+    });
+    let link = document.createElement("h6");
+    link.innerText = "More Info";
+    eventLink.appendChild(link);
+    let organisation = document.createElement("p");
+    organisation.className = "event_organisation";
+    organisation.innerHTML = `<b>Organisation: ${res_file[data].organisation}`;
+    let loc = document.createElement("h5");
+    let eventStatus = res_file[data].location;
+    setEventStatus();
+    loc.innerText = eventStatus;
+    eventDetails.append(startDate, endDate, organisation, loc, eventLink);
+    eventCard.append(eventTitle, eventDetails);
+    emptyDiv.appendChild(eventCard);
+    cardContainer.appendChild(emptyDiv);
+
+    //function to set the status of event
+    function setEventStatus() {
+      let endDate = res_file[data].end.split("/", 3);
+      endDate = `${endDate[1]}/${endDate[0]}/${endDate[2]}`;
+      endDate = new Date(endDate);
+      if (new Date() > endDate) {
+        eventStatus = "Offline";
+        loc.className = "locationOffline";
+      } else {
+        loc.className = "locationOnline";
+      }
+    }
+  }
+}
+
 // Fetch data from events.json file
 fetch("./Frontend/data/events.json")
   .then((res) => res.json())
-  .then((res) => {
-    let cardContainer = document.getElementsByClassName("card-container")[0];
-    for (let data in res) {
-      let emptyDiv = document.createElement("div");
-      emptyDiv.className = "empty_div";
-      let eventCard = document.createElement("div");
-      eventCard.className = "event_card";
-      let eventTitle = document.createElement("div");
-      let heading = document.createElement("h3");
-      heading.innerText = res[data].title;
-      heading.className = "event_title";
-      eventTitle.appendChild(heading);
-      let startDate = document.createElement("span");
-      startDate.className = "date";
-      startDate.innerHTML = `<b>Starts:</b> ${res[data].start}`;
-      let endDate = document.createElement("span");
-      endDate.className = "date";
-      endDate.innerHTML = `<b>Ends:</b> ${res[data].end}`;
-      let eventDetails = document.createElement("div");
-      eventDetails.className = "event_details";
-      let eventLink = document.createElement("a");
-      Object.assign(eventLink, {
-        href: `https://${res[data].website}`,
-        target: "_blank",
-        rel: "noopener noreferrer",
-        className: "btn btn-primary link",
-      });
-      let link = document.createElement("h6");
-      link.innerText = "More Info";
-      eventLink.appendChild(link);
-      let organisation = document.createElement("p");
-      organisation.className = "event_organisation";
-      organisation.innerHTML = `<b>Organisation: ${res[data].organisation}`;
-      let loc = document.createElement("h5");
-      let eventStatus = res[data].location;
-      setEventStatus();
-      loc.innerText = eventStatus;
-      eventDetails.append(startDate, endDate, organisation, loc, eventLink);
-      eventCard.append(eventTitle, eventDetails);
-      emptyDiv.appendChild(eventCard);
-      cardContainer.appendChild(emptyDiv);
-
-      //function to set the status of event
-      function setEventStatus() {
-        let endDate = res[data].end.split("/", 3);
-        endDate = `${endDate[1]}/${endDate[0]}/${endDate[2]}`;
-        endDate = new Date(endDate);
-        if (new Date() > endDate) {
-          eventStatus = "Offline";
-          loc.className = "locationOffline";
-        } else {
-          loc.className = "locationOnline";
-        }
-      }
-    }
-  });
+  .then((res) => { 
+    loadJSON(res);
+});
 
 // search an event
-let search = document.querySelector(".form-control");
-search.addEventListener("keyup", searchTerm);
-//function to calculate the similarity score between two strings
-function similarity(s1, s2) {
-      var longer = s1;
-      var shorter = s2;
-      if (s1.length < s2.length) {
-        longer = s2;
-        shorter = s1;
-      }
-      var longerLength = longer.length;
-      if (longerLength == 0) {
-        return 1.0;
-      }
-      return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
-    }
 
-    function editDistance(s1, s2) {
+//functions to calculate the similarity score between two strings
+
+function editDistance(s1, s2) {
       s1 = s1.toLowerCase();
       s2 = s2.toLowerCase();
 
@@ -101,25 +96,61 @@ function similarity(s1, s2) {
           costs[s2.length] = lastValue;
       }
       return costs[s2.length];
+}
+
+function similarity(s1, s2) {
+      var longer = s1;
+      var shorter = s2;
+      if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+      }
+      var longerLength = longer.length;
+      if (longerLength == 0) {
+        return 1.0;
+      }
+      return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function sortJSON(res_file, input)
+{
+  for(let data in res_file)
+  {
+    if(res_file[data].title.toLowerCase().indexOf(input) != -1)
+    {
+      res_file[data].score = 1;
     }
+    else {
+      res_file[data].score = similarity(res_file[data].title.toLowerCase(),input.toLowerCase());
+    }
+  }
+  res_file = res_file.filter(function (a){return a.score >= 0.08});
+  res_file.sort(function(a,b){return b.score - a.score });
+  loadJSON(res_file);
+
+}
+
+
+
+let search = document.querySelector(".form-control");
+
+search.addEventListener("keyup", searchTerm);
 
 //function to search the event
 function searchTerm(e) {
-  let eventList = document.querySelectorAll(".empty_div");
-  let input = e.target.value.toLowerCase();
-  if (input !="") {
-    
-    Array.from(eventList).forEach((eventItem) => {
-      let toSearch = eventItem.childNodes[0].children[0].childNodes[0].innerText;
-      console.log(similarity(toSearch.toLowerCase(), input));
-      console.log(toSearch.toLowerCase());
-      if ( toSearch.toLowerCase().indexOf(input) != -1||similarity(toSearch.toLowerCase(), input) > 0.08 ) {
-        eventItem.style.display = "block";
-      } else {
-        eventItem.style.display = "none";
-      }
-    });
-  }
+
+  fetch("./Frontend/data/events.json")
+  .then((res) => res.json())
+  .then((res_file) => { 
+    let input = e.target.value.toLowerCase();
+    if (input !="")
+    {
+      sortJSON(res_file,input);
+    }
+    else
+      loadJSON(res_file);
+});
+
 }
 
 const toggleSwitch = document.querySelector(".custom-control-input");
