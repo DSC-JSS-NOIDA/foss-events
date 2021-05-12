405ae76b11923ebcb0192fddaf86f79b52711684
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-05-12 20:56:59 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const request = require('request');
 const PORT = process.env.PORT || 3000;
 
 dotenv.config({ path: ".env" });
@@ -11,10 +16,22 @@ const app = new express();
 require("./db/db")();
 
 //middlewares
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.use(morgan("dev"));
 app.use(express.json());
 
 //routes
+app.get('/',(req,res)=>{
+    res.render('index');
+});
 app.use("/event", require("./routes/EventRoutes"));
 app.use("/users/logout", require("./routes/users/logout"));
 app.use("/users/login", require("./routes/users/login"));
